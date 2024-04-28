f2de18a498856697d168abcd00150ec040a39650
plee <phyllizl@gmail.com>
2024-04-28 15:48:36 +0100

    ensuring it can be distirbuted, electronstore intialization

--- main.js
+++ main.js
@@ -73,8 +73,25 @@ const createWindow = () => {
     });
     singleBoxWindow.loadFile('dist/index.html', { hash: `box/1` });    
     // singleBoxWindow.webContents.openDevTools();
-    
-
+    if (store.get("initialized") !== true){
+      store.set({
+        initialized: true,
+        storeLayout: [
+            { i: "note1", x: 2, y: 2, w: 2, h: 5, isResizable: true, resizeHandles: ["se"]},
+            { i: "link1", x: 2, y: 3, w: 1, h: 2, isResizable: false},
+            { i: "file1", x: 2, y: 4, w: 1, h: 2, isResizable: false}
+        ],
+        storeNotes:[
+            {id: "note1", content: "content for note 1"}
+        ],
+        storeLinks: [
+            {id: "link1", linkName: "GoogleTest", url: "https://www.google.com"},
+        ],
+        storeFiles: [
+            {id: "file1", fileName: "fakefilepath", filePath: String.raw`"C:\Users\ohcst\OneDrive\Desktop\testworddoc.docx"`}
+        ]    
+      });
+    }
   })
 
   app.on('window-all-closed', () => {
@@ -120,38 +137,12 @@ const createWindow = () => {
   });
 
 
-// See format for initial test data, found in "C:\Users\ohcst\AppData\Roaming\just-a-box-v2\config.json". Edit from there
-// store.set({
-//   storeLayout: [
-//       { i: "test", x: 0, y: 0, w: 2, h: 5, isResizable: true, resizeHandles: ["se"]},
-//       { i: "note1", x: 2, y: 2, w: 2, h: 5, isResizable: true, resizeHandles: ["se"]},
-//       { i: "link1", x: 2, y: 3, w: 1, h: 2, isResizable: false},
-//       { i: "file1", x: 2, y: 4, w: 1, h: 2, isResizable: false}
-//   ],
-//   storeNotes:[
-//       {id: "note1", content: "content for note 1"}
-//   ],
-//   storeLinks: [
-//       {id: "link1", linkName: "GoogleTest", url: "https://www.google.com"},
-//   ],
-//   storeFiles: [
-//       {id: "file1", fileName: "testdoc", filePath: String.raw`"C:\Users\ohcst\OneDrive\Desktop\testworddoc.docx"`}
-//   ]    
-// });
+// See format for initial test data, found in "C:\Users\ohcst\AppData\Roaming\just-a-box-v2\config.json".
+// or /Users/thomasoh/Library/Application Support/just-a-box-v2/config.json
+// Edit from there
 
 // {
 //   "storeLayout": [
-//       {
-//           "i": "test",
-//           "x": 0,
-//           "y": 0,
-//           "w": 2,
-//           "h": 5,
-//           "isResizable": true,
-//           "resizeHandles": [
-//               "se"
-//           ]
-//       },
 //       {
 //           "i": "note1",
 //           "x": 2,
